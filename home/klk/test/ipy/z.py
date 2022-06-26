6d274b03725375e6e3f05cff03a5da83715c316d
github.com/users/qgbcs <36606669+qgbcs@users.noreply.github.com>
2022-06-26 19:19:55 +0800

    2022-06-26__11.19.54__.910 <2.896 KiB>/home/klk/test/ipy/z.py

---
+++ home/klk/test/ipy/z.py
@@ -0,0 +1,46 @@
+#coding=utf-8
+import sys;'qgb.U' in sys.modules or sys.path.append('/home/klk/');from qgb import *
+#ws=F.dl(F.ls(U.gst)[-1])                                               # 1 
+#from qgb import *                                                      # 2 
+#ws=F.dl(F.ls(U.gst)[-1])                                               # 3 
+#_4=len(ws)                                                             # 4 
+#_5='https://github.com/python-pillow/Pillow/blob/master/src/_imagingft.c'  # 5 
+#h=N.get(_)                                                             # 6 
+#_7=T.regexMatchAll(h,r'[a-z.:/]*github/\w.\-=,]*')                     # 7 
+#_8=T.find(h,'github',c=9)                                              # 8 
+#for i in range(U.imax):
+    b                                                                   # 9 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    b                                                                   # 10 
+#_11=U.disable_log()                                                    # 11 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    b                                                                   # 12 
+#_13=r                                                                  # 13 
+#_14=r                                                                  # 14 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    b                                                                   # 15 
+#_16=r                                                                  # 16 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    N.HTTP.post("https://klk.pythonanywhere.com/-r=q.get_data().decode('utf-8')",123)  # 17 
+#get_ipython().magic('pinfo N.post')                                    # 18 
+#get_ipython().magic('pinfo N.HTTP.post')                               # 19 
+#N.HTTP.post("https://klk.pythonanywhere.com/-r=q.get_data().decode('utf-8')",123)  # 20 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    N.HTTP.post("https://klk.pythonanywhere.com/-r=q.get_data().decode('utf-8')",data=123)
+    b                                                                   # 21 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    N.HTTP.post("https://klk.pythonanywhere.com/-r=q.get_data().decode('utf-8')",data=123)
+    b                                                                   # 22 
+#while True:
+    r=N.get("https://klk.pythonanywhere.com/-p.set_data(a)")
+    N.HTTP.post("https://klk.pythonanywhere.com/-r=q.get_data().decode('utf-8')",data=123)
+    b                                                                   # 23 
+#_24=ipy.save(0)                                                        # 24 
+#ipy.save(1)                                                            # 25 
+#ipy.save('z')                                                          # 26 
