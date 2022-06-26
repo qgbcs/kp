f916ce3675801d60aec40d1bd65f589b0e6e74f7
github.com/users/qgbcs <36606669+qgbcs@users.noreply.github.com>
2022-06-26 19:32:53 +0800

    2022-06-26__11.32.53__.622 <4.296 KiB>/home/klk/test/ipy/T.regexMatchAll(h,r'[a-z.：／]＊gstatic.com[／＼w.＼-=,]＊').py

---
+++ home/klk/test/ipy/T.regexMatchAll(h,r'[a-z.：／]＊gstatic.com[／＼w.＼-=,]＊').py
@@ -0,0 +1,60 @@
+#coding=utf-8
+import sys;'qgb.U' in sys.modules or sys.path.append('/home/klk/');from qgb import *
+#get_ipython().system('curl gstatic.com')                               # 1 
+#get_ipython().system('curl -vvi ')                                     # 2 
+#get_ipython().system('curl -vvi https://translate.google.cn/')         # 3 
+#get_ipython().system('curl -vvi https://translate.google.com/')        # 4 
+#_5=_i4[9:]                                                             # 5 
+#_6=_i4[11:]                                                            # 6 
+#h=N.get(u)                                                             # 7 
+#from qgb import *                                                      # 8 
+#h=N.get(u)                                                             # 9 
+#h=N.get(_)                                                             # 10 
+#_11=len(h)                                                             # 11 
+#_12='gstatic' in h                                                     # 12 
+#_13=('gstatic')                                                        # 13 
+#_14=h.find('gstatic')                                                  # 14 
+#_15=h.find('gstatic4')                                                 # 15 
+#_16=h.find('gstatic')                                                  # 16 
+#_17=h.find('gstatic',904)                                              # 17 
+#_18=h.find('gstatic',905)                                              # 18 
+#_19=h.find('gstatic',905+1)                                            # 19 
+#_20=h.find('gstatic',-1)                                               # 20 
+#_21=h.find('gstatic',-678900)                                          # 21 
+#_22=U.cdqp();
+get_ipython().system('git pull origin master')
+#U.cdb(p=1)
+U.r(U,T,N,N.HTTP,F,py)
+U.cdb()                                                                 # 22 
+#_23=T.find(h,'gstatic')                                                # 23 
+#_24=len(_)                                                             # 24 
+#_25=T.find(h,'gstatic',c=5)                                            # 25 
+#_26=T.find(h,'gstatic',c=9)                                            # 26 
+#_27=T.regexMatchAll(h,'\w*gstatic.com')                                # 27 
+#_28=T.regexMatchAll(h,'\W*gstatic.com')                                # 28 
+#_29=T.regexMatchAll(h,'[a-z.]*gstatic.com')                            # 29 
+#_30=T.regexMatchAll(h,'[a-z.]*gstatic.com[/a-z]*')                     # 30 
+#_31=_26[-5:]                                                           # 31 
+#_32=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/a-z]*')                   # 32 
+#_33=len(_)                                                             # 33 
+#_34=_26[-9]                                                            # 34 
+#_35=_26[-13]                                                           # 35 
+#_36=h[296076:296076+99]                                                # 36 
+#_37=h[296076:296076+99]                                                # 37 
+#_38=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/a-z\n]*')                 # 38 
+#_39=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/a-z0-9]*')                # 39 
+#_40=h[296076:296076+99]                                                # 40 
+#_41=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/\w]*')                    # 41 
+#_42=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/\w.]*')                   # 42 
+#_43=ipy.save(_i)                                                       # 43 
+#_44=_26[-11]                                                           # 44 
+#n=307563                                                               # 45 
+#_46=h[n:n+99]                                                          # 46 
+#_47=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/\w.-]*')                  # 47 
+#_48=h[n:n+99]                                                          # 48 
+#_49=T.regexMatchAll(h,'[a-z.:/]*gstatic.com[/\w.-=]*')                 # 49 
+#_50=T.regexMatchAll(h,r'[a-z.:/]*gstatic.com[/\w.\-=]*')               # 50 
+#_51=h[n:n+199]                                                         # 51 
+#_52=T.regexMatchAll(h,r'[a-z.:/]*gstatic.com[/\w.\-=,]*')              # 52 
+#_53=h[n:n+199]                                                         # 53 
+#ipy.save(_i52)                                                         # 54 
